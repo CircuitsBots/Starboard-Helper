548acb60ffb694aa3a5710932aa11a635f405080
Lucas <circuitsacul@gmail.com>
2020-10-09 10:30:33 -0400

    Revert "Remove about starboard"
    This reverts commit a094e161c3995e5f990288a6a29ab11465ed2744.

--- sentences.py
+++ sentences.py
@@ -11,6 +11,11 @@ triggers = {
     "The bot isn't working": 2,
     "Starboard won't respond": 2,
     "The bot won't respond": 2,
+    "What can Starboard do": 3,
+    "What can this bot do": 3,
+    "What does Starboard do": 3,
+    "What does this bot do": 3,
+    "What is a starboard": 3,
     "How do I use starboard": 4,
     "How do I use this bot": 4,
     "How do I setup starboard": 4,
@@ -28,7 +33,7 @@ responses = {
     0: "Please post your question and be patient.",
     1: "The default prefix for Starboard is `sb!` or its mention.",
     2: "Please elaborate on your issue and what you have tried.\nYou can also look here for troubleshooting <https://github.com/CircuitsBots/Starboard/wiki/Troubleshooting>",
-    3: None,
+    3: "A starboard is a bot that allows users to star messages by reacting to them with :star:. When a message receives a certain number of stars, it is reposted in a channel usually called #starboard.",
     4: "Please look at the quick start guide here: <https://github.com/CircuitsBots/Starboard/wiki/Quick-Setup>",
     5: "You can invite Starboard to your server with this link: <https://discord.com/api/oauth2/authorize?client_id=700796664276844612&permissions=388160&scope=bot>"
 }
