00223790157dce36db0dfa478d216ea28394c918
Lucas <circuitsacul@gmail.com>
2020-10-09 14:21:26 -0400

    Improvements

--- sentences.py
+++ sentences.py
@@ -33,14 +33,26 @@ triggers = {
     "How do I invite the bot": 5,
     "How do I add starboard": 5,
     "How do I add the bot": 5,
-    "Starboard invite link": 5
+    "Starboard invite link": 5,
+    "Is starboard offline": 6,
+    "Is the bot offline": 6,
+    "Can I change the prefix": 7,
+    "How do I change the prefixes": 7,
+    "How do I change the prefix": 7,
+    "Is the prefix customizable": 7,
+    "Can I have custom prefixes": 7,
+    "Are the prefixes customizable": 7,
+    "How do I customize the prefix": 7,
+    "How do I customize the prefixes": 7
 }
 
 responses = {
-    0: "Please post your question and be patient.",
+    0: "What do you need help with?",
     1: "The default prefix for Starboard is `sb!` or its mention.",
     2: "Please elaborate on your issue and what you have tried.\nYou can also look here for troubleshooting <https://github.com/CircuitsBots/Starboard/wiki/Troubleshooting>",
     3: "A starboard is a bot that allows users to star messages by reacting to them with :star:. When a message receives a certain number of stars, it is reposted in a channel usually called #starboard.",
     4: "Please look at the quick start guide here: <https://github.com/CircuitsBots/Starboard/wiki/Quick-Setup>",
-    5: "You can invite Starboard to your server with this link: <https://discord.com/api/oauth2/authorize?client_id=700796664276844612&permissions=388160&scope=bot>"
+    5: "You can invite Starboard to your server with this link: <https://discord.com/api/oauth2/authorize?client_id=700796664276844612&permissions=388160&scope=bot>",
+    6: "If the bot is offline, it will probably be back soon. Please be patient.",
+    7: "You can see one of starboards prefixes by mentioning it. From there, you can run `[p]prefix add <prefix>` or `[p]prefix remove <prefix>` to customize them."
 }
